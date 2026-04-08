61631669e7476fdd70b3201b07c8a31526e1df17
shaxbanovamir <shaxbanovamir@gmail.com>
2026-04-08 03:07:55 +0300

    Create auth.js file in root directory

---
+++ auth.js
@@ -0,0 +1,12 @@
+// Authentication logic for the course platform
+
+const auth = {
+    login: function(username, password) {
+        // Login logic
+    },
+    logout: function() {
+        // Logout logic
+    },
+};
+
+module.exports = auth;
